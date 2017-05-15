cbfdfd8927e3238840e35cf45709f7b4a213e033
Ralf Wieland <rwieland@zalf.de>
2017-05-15 08:26:06 +0200

    argparse in hasse_csv.py

--- stats/hasse_csv.py
+++ stats/hasse_csv.py
@@ -1,29 +1,60 @@
 #!/usr/bin/env python
 
 import hasse as hd
+import argparse
+
+""" new user friendly version based on argparse """
 
 def main():
+    parser = argparse.ArgumentParser(description='HASSE')
+    parser.add_argument('-f', help='csv-data filename',required=True)
+    parser.add_argument('-m', default='hasse',
+                        choices=['hasse', 'majorization', 'm2', 'majo'],
+                        help='select a comparison method')
+    parser.add_argument('-d', default='hasse',
+                        choices=['hasse','simple'],
+                        help='select a visualisation method: hasse/simple'
+                        )
+    args = parser.parse_args()
     #read data from file
-    mw,z_namen=hd.read_data('test.csv')
-    # mw,z_namen=hd.read_data('data2.txt')
+    
+    #mw,z_namen=hd.read_data('test.csv')
+    #mw,z_namen=hd.read_data('data2.txt')
+    #mw,z_namen=hd.read_data('Bawue1.txt')
+    #mw,z_namen=hd.read_data('BawueRheinks.txt')
+    filename=args.f
+    mw,z_namen=hd.read_data(filename)
+    for i in range(len(mw)):
+	print z_namen[i],
+        for j in range(len(mw[i])):
+	    print mw[i,j],
+        print
+    
+    
     for i in range(len(mw)):
 	print z_namen[i],
         for j in range(len(mw[i])):
 	    print mw[i,j],
         print
     # define a hassetree
-    # hasse1=hd.hassetree()
-    # hasse1=hd.hassetree(hd.majorization_comp)
-    # hasse1=hd.hassetree(hd.majo_comp)
-    hasse1=hd.hassetree(hd.m2_comp)
+    if(args.m=='hasse'):
+        hasse1=hd.hassetree(hd.hasse_comp)
+    if(args.m=='majorization'):
+        hasse1=hd.hassetree(hd.majorization_comp)
+    if(args.m=='majo'):
+        hasse1=hd.hassetree(hd.majo_comp)
+    if(args.m=='m2'):
+        hasse1=hd.hassetree(hd.m2_comp)
     # fill it from mw
     for i in range(len(mw)):
         sitp=hd.sit(z_namen[i],mw[i])
         hasse1.insert(sitp)
-    hasse1.print_eq()
-    gx,level=hasse1.make_graph()
-    # print HD
-    hd.print_hd(gx,level,'test')
-
+    hasse1.col_norm()
+    if(args.d=='hasse'):
+        gx,level=hasse1.make_graph()
+        hd.print_hd(gx,level,args.m)
+    if(args.d=='simple'):
+       hasse1.draw_simple(args.m)
+       
 if __name__ == "__main__":
     main()
