4119f6f209291ea8bd4214c8f471ab6d530cdaf3
Ralf <rwieland@zalf.de>
2019-01-16 16:00:15 +0100

    small adaptation

--- tests/test_distances.py
+++ tests/test_distances.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # simple test for voronoi maps
 
 import numpy as np
--- tests/test_interpol.py
+++ tests/test_interpol.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # simple test for different interpolation methods
 
 import numpy as np
